580ef221bb1bd800f13186030a547c58f40ebc16
Alex <alexfidalgo10@gmail.com>
2023-11-16 19:33:30 -0300

    displays automata visually correctly

--- cellular_automata.py
+++ cellular_automata.py
@@ -1,29 +1,52 @@
-import matplotlib.pyplot as plt
-import numpy as np
+import pygame
+import random
+import time
 
 class CellularAutomaton:
+
     def __init__(self, N):
-        self.N = N  # lattice_size
-        self.cells = np.zeros(N)
-        self.evolve_history = []  # List to store the evolution history
+        self.N = N
+        self.height = 50
+        self.cells = [[random.choice([0, 1]) for _ in range(self.N)] for _ in range(self.height)]
+        self.history = [None] * self.height
 
-    def visualize_cells(self):
-        plt.imshow(self.evolve_history, cmap='gray_r', aspect='auto', extent=(0, self.N, 0, len(self.evolve_history)))
-        plt.pause(1)
+    def draw_cells(self, screen):
+        for i, row in enumerate(self.cells):
+            for j, cell in enumerate(row):
+                if self.history[i] is None:
+                    color = (220, 220, 220)
+                else:
+                    color = (0, 0, 0) if cell == 0 else (255, 255, 255)
+                pygame.draw.rect(screen, color, (j * 10, i * 10, 10, 10))
 
-    def randomize_cells(self, density=0.5):
-        self.cells = np.random.choice([0, 1], size=self.N, p=[1 - density, density])
+    def randomize(self):
+        new_cells = [[random.choice([0, 1]) for _ in range(self.N)] for _ in range(self.height)]
+        self.cells = new_cells
+        # self.history.pop()
+        self.history.insert(0, new_cells)
 
-    def evolve_and_visualize(self, generations):
-        for _ in range(generations):
-            self.evolve()
-            self.evolve_history.append(np.copy(self.cells))  # Store the current state for visualization
-            self.visualize_cells()
+    def simulate(self, generations):
+        pygame.init()
+        screen = pygame.display.set_mode((self.N*10, self.height*10))
+        pygame.display.set_caption('Cellular Automaton')
 
-    def evolve(self, density=0.5):
-        self.cells = np.random.choice([0, 1], size=self.N, p=[1 - density, density])
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
 
-# Example usage:
-automaton = CellularAutomaton(N=100)
-automaton.randomize_cells(density=0.3)
-automaton.evolve_and_visualize(generations=100)
+            self.draw_cells(screen)
+            self.randomize()
+            pygame.display.flip()
+
+            time.sleep(1)
+
+            if len(self.history) > generations:
+                self.history.pop()
+
+        pygame.quit()
+
+if __name__ == '__main__':
+    automaton = CellularAutomaton(N=10)
+    automaton.simulate(generations=100)
