70bf5e8b6e560614e0642cd237c7a38de4a8d9fa
Alex <alexfidalgo10@gmail.com>
2023-11-17 15:15:14 -0300

    creates config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+class Config:
+    
+    N = 100  # Size of the lattice
+    density_threshold = 0.5  # Density threshold for cell initialization
+    generations = 20  # Number of generations to evolve
+
+    cell_size = 20
+    delay = 0.1 # ms
+
