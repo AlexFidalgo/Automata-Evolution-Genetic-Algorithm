a41f5cd4a02461dc35b11be3724967c3f8b91e58
Alex <alexfidalgo10@gmail.com>
2023-11-18 01:56:15 -0300

    gets all white or black

--- rule_functions.py
+++ rule_functions.py
@@ -5,3 +5,15 @@ def randomize(N):
     new_cells = [random.choice([0, 1]) for _ in range(N)]
 
     return new_cells
+
+def get_all_black(N):
+
+    new_cells = [1 for _ in range(N)]
+
+    return new_cells
+
+def get_all_white(N):
+
+    new_cells = [0 for _ in range(N)]
+
+    return new_cells
