93b7c6213bed417b626f3cb771123d4a18802597
Alex <alexfidalgo10@gmail.com>
2023-11-19 00:09:44 -0300

    history cell parameter

--- rule_functions.py
+++ rule_functions.py
@@ -1,18 +1,18 @@
 import random
 
-def randomize(N):
+def randomize(N, current_cells):
 
     new_cells = [random.choice([0, 1]) for _ in range(N)]
 
     return new_cells
 
-def get_all_black(N):
+def get_all_black(N, current_cells):
 
     new_cells = [1 for _ in range(N)]
 
     return new_cells
 
-def get_all_white(N):
+def get_all_white(N, current_cells):
 
     new_cells = [0 for _ in range(N)]
 
