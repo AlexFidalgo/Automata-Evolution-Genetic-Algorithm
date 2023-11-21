eda0768decd793d416d16ab3e0b9d38826949dc6
Alex <alexfidalgo10@gmail.com>
2023-11-21 17:53:04 -0300

    draws ic from uniformly distributed over 0,1

--- initialization.py
+++ initialization.py
@@ -1,13 +1,26 @@
 import random
 
-def get_initial_configuration(N, ic = 'random'):
-
+def get_initial_configuration(N, ic='random'):
+    
     if ic == 'random':
         return [[random.choice([0, 1]) for _ in range(N)]]
 
     elif ic == 'uniformly distributed':
-        #randomly choosing ICs that are uniformly distributed over ρ ∈ [0.0, 1.0], with exactly half with ρ < ρc and half with ρ > ρc
-        return None
+        # Randomly choosing IC uniformly distributed over ρ ∈ [0.0, 1.0]
+        density = random.uniform(0.0, 1.0)
+        ones_count = int(N * density)
+        zeros_count = N - ones_count
+        initial_config = [1] * ones_count + [0] * zeros_count
+        random.shuffle(initial_config)
+        density_real = ones_count/(zeros_count + ones_count)
+        return [initial_config]
 
     else:
         return [ic]
+
+
+if __name__ == '__main__':
+
+    N = 50
+    initial_config_random = get_initial_configuration(N, ic='random')
+    initial_config_uniform = get_initial_configuration(N, ic='uniformly distributed')
