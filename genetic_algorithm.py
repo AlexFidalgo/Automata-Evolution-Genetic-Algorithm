a6232f4e1bedf139f8fe519b32488172f2e29a1c
Alex <alexfidalgo10@gmail.com>
2023-11-23 00:51:41 -0300

    chromosome class

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -1,7 +1,17 @@
 from config import Config
+from rule_functions import *
 
 class Population:
 
-    def __init__:
+    def __init__():
 
         self.chromosomes
+
+
+class Chromosome:
+
+    def __init__(self, N, r):
+
+        self.N = N
+        self.r = r
+        self.dna = get_rule_from_flat_distribution(N, r)
