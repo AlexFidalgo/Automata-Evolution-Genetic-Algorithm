53e6b233f5b061763c3e97760242c51c6ad5a943
Alex <alexfidalgo10@gmail.com>
2023-11-23 00:48:05 -0300

    get_rule_from_flat_distribution

--- rule_functions.py
+++ rule_functions.py
@@ -9,7 +9,7 @@ def randomize(current_cells):
 
     return new_cells
 
-def get_all_black(current_cells):
+def get_all_black(current_cells): #last wolfram_rule
 
     N = len(current_cells)
 
@@ -17,7 +17,7 @@ def get_all_black(current_cells):
 
     return new_cells
 
-def get_all_white(current_cells):
+def get_all_white(current_cells): #first wolfram_rule
 
     N = len(current_cells)
 
@@ -46,6 +46,31 @@ def get_wolfram_rule(current_cells, rule = 110, r = 1):
         new_cells.append(rule_dict[input_str])
         
     return new_cells
+
+def get_rule_from_flat_distribution(N, r):
+    
+    rule_size = 2**get_total_number_of_cells(r)
+
+    list_of_candidate_rules = []
+
+    # Iterate over all possible densities and assign a random rule for each
+    for density in range(rule_size + 1):
+        # Create a rule with the desired density
+        ones_count = density
+        zeros_count = rule_size - density
+        rule = [1] * ones_count + [0] * zeros_count
+
+        # Randomly shuffle the rule
+        random.shuffle(rule)
+
+        list_of_candidate_rules.append(rule)
+        
+    selected_rule = random.choice(list_of_candidate_rules)
+
+    # rule_dict = {format(i, '0' + str(2*r + 1) + 'b'): selected_rule[i] 
+    #             for i in range(rule_size)}
+
+    return selected_rule
 
     
     
