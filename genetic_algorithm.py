031ae0588e67a72c230d1f2344a4e446e8b695b7
Alex <alexfidalgo10@gmail.com>
2023-11-24 07:03:09 -0300

    creates Chromosome as a subclass

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -1,5 +1,7 @@
+from cellular_automata import CellularAutomaton
 from config import Config
 from rule_functions import *
+from initialization import *
 
 class Population:
 
@@ -8,10 +10,31 @@ class Population:
         self.chromosomes
 
 
-class Chromosome:
 
-    def __init__(self, N, r):
 
-        self.N = N
-        self.r = r
+class Chromosome(CellularAutomaton):
+
+    def __init__(self, N, r, ic_function=get_random_ic, **kwargs):
+
+        super().__init__(N, ic_function, **kwargs)
         self.dna = get_rule_from_flat_distribution(N, r)
+
+    def show_rule(self):
+
+        rule_size = 2**get_total_number_of_cells(r)
+
+        rule_dict = {format(i, '0' + str(2*r + 1) + 'b'): self.dna[i] 
+                    for i in range(rule_size)}
+
+        print(rule_dict)
+
+
+
+if __name__ == '__main__':
+
+    N = 50
+    r = 1
+    
+    c = Chromosome(N, r)
+
+    c.show_rule()
