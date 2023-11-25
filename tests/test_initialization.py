46cb581f54c2186b2eeec1a95c2cbd2e7e42720b
Alex <alexfidalgo10@gmail.com>
2023-11-25 14:23:08 -0300

    test_get_uniformly_distributed_ic

---
+++ tests/test_initialization.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from initialization import *
+
+def test_get_uniformly_distributed_ic(num_iterations, N=51):
+    densities = []
+
+    for _ in range(num_iterations):
+        initial_config = get_uniformly_distributed_ic(N)[0]
+        density_of_ones = sum(initial_config) / N
+        densities.append(density_of_ones)
+
+    # Plotting the distribution
+    plt.hist(densities, bins=20, edgecolor='black')
+    plt.title('Distribution of Densities in Initial Configurations')
+    plt.xlabel('Density of Ones')
+    plt.ylabel('Frequency')
+    plt.show()
+
+num_iterations = 1000
+test_get_uniformly_distributed_ic(num_iterations)
