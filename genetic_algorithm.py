601f9d8b044152b00b6e6bc780488130728465af
Alex <alexfidalgo10@gmail.com>
2023-11-25 14:10:50 -0300

    creates chromosome specific functions

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -1,7 +1,6 @@
 from cellular_automata import CellularAutomaton
-from config import Config
-from rule_functions import *
 from initialization import *
+from rule_functions import *
 
 class Population:
 
@@ -16,17 +15,32 @@ class Chromosome(CellularAutomaton):
 
     def __init__(self, N, r, ic_function=get_random_ic, **kwargs):
 
-        super().__init__(N, ic_function, **kwargs)
-        self.dna = get_rule_from_flat_distribution(N, r)
+        super().__init__(N, r, ic_function, **kwargs)
+        self.dna = get_rule_from_flat_distribution(r)
+
+        self.initialize_rule_dict()
+
+    def set_rule_dict(self):
+
+        rule_size = 2**get_total_number_of_cells(self.r)
+
+        self.rule_dict = {format(i, '0' + str(2*self.r + 1) + 'b'): self.dna[i] 
+            for i in range(rule_size)}
+
+    def initialize_rule_dict(self):
+
+        rule_size = 2**get_total_number_of_cells(self.r)
+
+        self.rule_dict = {format(i, '0' + str(2*self.r + 1) + 'b'): self.dna[i] 
+                                for i in range(rule_size)}
+
+
+    def get_dna_rule(self):
+
+        current_cells = self.history[-1][:]
 
     def show_rule(self):
-
-        rule_size = 2**get_total_number_of_cells(r)
-
-        rule_dict = {format(i, '0' + str(2*r + 1) + 'b'): self.dna[i] 
-                    for i in range(rule_size)}
-
-        print(rule_dict)
+        print(self.rule_dict)
 
 
 
@@ -38,3 +52,10 @@ if __name__ == '__main__':
     c = Chromosome(N, r)
 
     c.show_rule()
+
+
+
+    # delay = Config.delay
+    # height = Config.height
+
+    # c.simulate(height = height, rule_function = get_all_black, delay = delay)
