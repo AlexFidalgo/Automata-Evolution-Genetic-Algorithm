752fbd2606e7bbf98d2145869684b99276d51e94
Alex <alexfidalgo10@gmail.com>
2023-11-29 12:11:03 -0300

    test rule function clean up

--- tests/test_rule_functions.py
+++ tests/test_rule_functions.py
@@ -22,28 +22,6 @@ def test_get_rule_from_flat_distribution(num_iterations, r):
         proportion = count / total_iterations
         print(f"{density}/{total_combinations}: {proportion:.4f}")
 
-# def plot_distribution_histogram(num_iterations):
-#     r = 1
-#     total_combinations = get_number_of_combination_of_bits(get_total_number_of_cells(r))
-#     counts = {i: 0 for i in range(total_combinations + 1)}
-
-#     for _ in range(num_iterations):
-#         rule = get_rule_from_flat_distribution(r)
-#         density_of_ones = sum(list(rule.values()))
-#         counts[density_of_ones] += 1
-
-#     total_iterations = sum(counts.values())
-
-#     # Calculate proportions
-#     proportions = [count / total_iterations for count in counts.values()]
-
-#     # Plot the histogram
-#     plt.bar(counts.keys(), proportions)
-#     plt.xlabel('Density of Ones')
-#     plt.ylabel('Proportion')
-#     plt.title(f'Distribution of Rules for {total_iterations} iterations')
-#     plt.show()
-
 def plot_distribution_histogram(num_iterations, r):
     ones_density_list = []
 
@@ -51,16 +29,16 @@ def plot_distribution_histogram(num_iterations, r):
         rule_dict = get_rule_from_flat_distribution(r)
         ones_density = list(rule_dict.values()).count(1) / len(rule_dict)
         ones_density_list.append(ones_density)
-
-    # Plotting the histogram
-    plt.hist(ones_density_list, bins='auto', alpha=0.7, color='b')
+        
+    bins= [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
+    plt.hist(ones_density_list, bins=100, color='blue', edgecolor='black')
     plt.title('Distribution of Ones Density')
     plt.xlabel('Ones Density')
     plt.ylabel('Frequency')
     plt.show()
 
-num_iterations = 10000
+num_iterations = 1000
 r = 3
-test_get_rule_from_flat_distribution(num_iterations, r)
-# plot_distribution_histogram(num_iterations, r)
+# test_get_rule_from_flat_distribution(num_iterations, r)
+plot_distribution_histogram(num_iterations, r)
 
